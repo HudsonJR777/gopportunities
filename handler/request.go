542c769b61e3856375fb4663dc93fdc30cb55aa1
HudsonJR777 <hudsonjunior@pkkgmail.com>
2026-05-08 15:04:01 -0300

    fix: add changes response swagger

--- handler/request.go
+++ handler/request.go
@@ -41,7 +41,7 @@ func (r *CreateOpeningRequest) Validate() error {
 	return nil
 }
 
-type UpadateOpeningRequest struct {
+type UpdateOpeningRequest struct {
 	Role     string `json:"role"`
 	Company  string `json:"company"`
 	Location string `json:"location"`
@@ -50,7 +50,7 @@ type UpadateOpeningRequest struct {
 	Salary   int64  `json:"salary"`
 }
 
-func (r *UpadateOpeningRequest) Validate() error {
+func (r *UpdateOpeningRequest) Validate() error {
 
 	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
 		return nil
